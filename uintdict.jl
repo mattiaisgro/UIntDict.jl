12817ecf5aecc8a8cc5caea48da259dbb7caaa8f
Mattia Isgrò <mattiaisgro@users.noreply.github.com>
2024-05-31 21:44:20 +0200

    Add vector allocation, Fix hash function

--- uintdict.jl
+++ uintdict.jl
@@ -8,13 +8,14 @@
 # Simple unsigned integer hash function with linear probing
 function hashuint(key::UInt, size::UInt; coll::UInt = 0)
 
-	return ((key % size) + coll) % size
+	return mod(mod(key, size) + coll, size) + 1
 end
 
 
 
-mutable struct UIntDict{K,V} <: AbstractDict{K,V} where K where V
+mutable struct UIntDict{K,V} <: AbstractDict{K, V}
 	
+
 	# Vector holding pairs
 	table::Vector{Tuple{K, V}}
 
@@ -28,28 +29,35 @@ mutable struct UIntDict{K,V} <: AbstractDict{K,V} where K where V
 	# Default constructor
 	function Dict{K,V}() where V where K
         size = 0
-        capacity = 0
-    	# table = empty vector
+        capacity = 32
+    	table = Vector{Tuple{K,V}}(undef, capacity)
     end
 
     # Copy constructor
     function UIntDict{K,V}(d::Dict{K,V}) where V where K
-    	# allocate table with same size
-        # copy pairs from d to this
+    	
+    	table = Vector{Tuple{K,V}}(undef, d.capacity)
+        
+    	for pairs in d.table
+    		# insert pair into table
+    	end
     end
     	
     # Initialization constructor
     function UIntDict{K, V}(vec::Vector{Tuple{K, V}}) where {K, V}
-        # allocate table
-        # insert pairs into table
+        
+    	table = Vector{Tuple{K,V}}(undef, length(vec))
+
+        for pair in vec
+        	# insert pair into table
+        end
     end
 
 
     # Initialization constructor
     function UIntDict{K, V}(pairs::Pair...) where {K, V}
         
-        # allocate table
-        # insert pairs into table
+        table = Vector{Tuple{K,V}}(undef, length(pairs))
 
         for pair in pairs
         	# insert pair into table
@@ -74,28 +82,33 @@ function get!(d::UIntDict, key::UInt, default)
 end
 
 
-function getkey(d::UIntDict, key::UInt, default)
-
-end
-
-
+# Delete the key value pair associated with the given key
 function delete!(d::UIntDict, key::UInt)
 
 end
 
 
+# Delete and then return the value associated with
+# the given key if it exists, otherwise return default
 function pop!(d::UIntDict, key::UInt; default)
 
 end
 
 
+# Set a key value pair in the dictionary
 function setindex!(d::UIntDict, val::UInt, key::UInt)
-
+	
+	# Find the key value pair if it already exists
+	# or iterate to find the first empty pair after collisions
 end
 
 
+# Get the index of a key value pair with
+# the given key
 function getindex(d::UIntDict, key::UInt)
 
+	# Iterate over the table by probing
+	# and comparing the key to the pair's key
 end
 
 
@@ -104,13 +117,14 @@ end
 function haskey(d::UIntDict, key::UInt)
 
 	# Iterate over possible locations of the key-value pair
-	# by considering hash and collisions
+	# by considering hash and collisions, up to n maximum collisions
 end
 
 
 function sizehint!(d::UIntDict, size)
 	
 	# Allocate at least <size> memory in d
+	# If d was already initialized, copy the new table
 end
 
 
