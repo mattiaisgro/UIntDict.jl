4d7cabb9179ef7ed3babf56562a60a827470d29b
Mattia Isgrò <mattiaisgro@users.noreply.github.com>
2024-06-04 21:30:38 +0200

    Add delete!(), pop!() and haskey()

--- uintdict.jl
+++ uintdict.jl
@@ -1,8 +1,15 @@
 
 #
-# @class UIntDict
+# UIntDict
 # An hash map with unsigned integer key and value
 #
+
+
+# Nearest primes to powers of 2
+global uint_sizes = [
+	31, 61, 127, 251, 509, 1021, 2039, 4093,
+	8191, 16381, 32749, 65521, 131071, 262139, 524287,
+	1048573, 2097143, 4194301 ]
 
 
 # Simple unsigned integer hash function with linear probing
@@ -33,10 +40,9 @@ mutable struct UIntDict{K,V} <: AbstractDict{K, V}
 			# ERROR: Keys are not unsigned integer type!
 		end
 
-        size = 0
-        capacity = 32
-    	table = Vector{Tuple{K,V}}(undef, capacity)
+        new(Vector{Tuple{K,V}}(undef, 32), 32, 0)
     end
+
 
     # Copy constructor
     function UIntDict{K,V}(d::Dict{K,V}) where V where K
@@ -51,8 +57,11 @@ mutable struct UIntDict{K,V} <: AbstractDict{K, V}
     	for pairs in d.table
     		# insert pair into table
     	end
+
+    	new(table, d.capacity, d.size)
     end
     	
+
     # Initialization constructor
     function UIntDict{K, V}(vec::Array{Tuple{K, V}}) where V where K
 
@@ -66,18 +75,32 @@ mutable struct UIntDict{K,V} <: AbstractDict{K, V}
         for pair in vec
         	# insert pair into table
         end
+
+        new(table, d.capacity, d.size)
     end
 
 
     # Initialization constructor
     function UIntDict{K, V}(pairs::Pair...) where {K, V}
         
-        table = Vector{Tuple{K,V}}(undef, length(pairs))
+    	d = UIntDict{K, V}()
+    	reallocate(d)
 
         for pair in pairs
-        	# insert pair into table
+        	insert!(d, pair.first, pair.second)
         end
+
+        return d
     end
+	
+end
+
+
+# Reallocate the hashmap with a reallocation strategy
+function reallocate(d::UIntDict)
+
+	# Choose primes near powers of 2 ?
+	sizehint!(d, 2 * d.capacity);
 	
 end
 
@@ -89,18 +112,21 @@ function insert!(d::UIntDict, key::UInt, value)
 
 		index = hashuint(key, coll)
 		
-		# The key-value pair was found
-		if(table[index].first == key)
-			table[index] = (key, value)
-		end
-
 		# No collision => the key doesn't exist
 		if(table[index].first == undef)
 			table[index] = (key, value)
+			return
+		end
+
+		# The key-value pair was found
+		if(table[index].first == key)
+			table[index] = (key, value)
+			return
 		end
 	end
 
 	# TO-DO Max collisions! Reallocate table immediately
+	reallocate(d)
 end
 
 
@@ -124,7 +150,6 @@ function get(d::UIntDict, key::UInt, default)
 		end
 	end
 
-	# TO-DO If all pairs collide, reallocate table immediately
 	return default
 end
 
@@ -153,7 +178,7 @@ function get!(d::UIntDict, key::UInt, default)
 		end
 	end
 
-	# TO-DO If all pairs collide, reallocate table immediately
+	reallocate(d)
 	return default
 end
 
@@ -161,6 +186,15 @@ end
 # Delete the key value pair associated with the given key
 function delete!(d::UIntDict, key::UInt)
 
+	for coll in 0:(length(d.table) - 1)
+		
+		index = hashuint(key, coll)
+
+		if d.table[index].first == key
+			d.table[index] = undef
+			break;
+		end
+	end
 end
 
 
@@ -168,10 +202,23 @@ end
 # the given key if it exists, otherwise return default
 function pop!(d::UIntDict, key::UInt; default)
 
+	for coll in 0:(length(d.table) - 1)
+
+		index = hashuint(key, coll)
+
+		if d.table[index].first == key
+			value = d.table[index].second
+			d.table[index] = undef
+			return value
+		end
+
+	end
+
+	return default;
 end
 
 
-# Set a key value pair in the dictionary
+# Set the value associated to a given key
 function setindex!(d::UIntDict, val::UInt, key::UInt)
 	
 	# Find the key value pair if it already exists
@@ -179,8 +226,7 @@ function setindex!(d::UIntDict, val::UInt, key::UInt)
 end
 
 
-# Get the index of a key value pair with
-# the given key
+# Get the value stored at the given key
 function getindex(d::UIntDict, key::UInt)
 
 	# Iterate over the table by probing
@@ -192,8 +238,21 @@ end
 # to the given key
 function haskey(d::UIntDict, key::UInt)
 
-	# Iterate over possible locations of the key-value pair
-	# by considering hash and collisions, up to n maximum collisions
+	for coll in 0:(length(d.table) - 1)
+		
+		# The key was found
+		if d.table[index].first == key
+			return true
+		end
+
+		# No collision => Key doesn't exist
+		if d.table[index] == undef
+			return false
+		end
+	end
+
+	# Max collisions
+	return false
 end
 
 
@@ -211,10 +270,6 @@ getkey(d::UIntDict, key::UInt, default) = haskey(d, key) ? key : default
 
 # Test UIntDict
 
-# Square a number
-function sqr(x)
-	x * x
-end
 
-# Construct a dictionary using a generator
 d = UIntDict{UInt, UInt}(1 => 2, 2 => 3)
+println(d.table)
